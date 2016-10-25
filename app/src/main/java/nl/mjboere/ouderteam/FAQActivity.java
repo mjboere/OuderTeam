80cfedebcd5ab8441df0d4091208ad8864ae4a38
MJBoere <mjboere33@gmail.com>
2016-10-25 17:10:19 +0200

    Initial commit - StartAct. + MainAct + FAQAct

---
+++ app/src/main/java/nl/mjboere/ouderteam/FAQActivity.java
@@ -0,0 +1,14 @@
+package nl.mjboere.ouderteam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FAQActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faq);
+        setTitle("FAQ");
+    }
+}
---
+++ app/src/main/java/nl/mjboere/ouderteam/MainActivity.java
@@ -0,0 +1,43 @@
+package nl.mjboere.ouderteam;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Button login = (Button) findViewById(R.id.faq_button);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Login knop ingedrukt", "Start LoginActivity");
+                startActivity(new Intent(MainActivity.this, FAQActivity.class));
+            }
+        });
+
+
+    }
+
+}
